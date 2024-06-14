8f8f5da56efee273eeef9da40101c30864a7d963
Knieciu <gabennewell00@gmail.com>
2024-06-14 19:18:52 +0200

    game works from local storage

--- typing-game/src/App.tsx
+++ typing-game/src/App.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const wordsOfCode: string[] = ["<div>", "<br>"];
+
+  const [input, setInput] = useState("");
+  const [game, setGame] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //console.log(e.target.value);
+    setInput(e.target.value);
+  };
+
+  const handleGameStart = (e: any) => {
+    console.log(game);
+    setGame(true);
+
+    if (currentIndex >= wordsOfCode.length) {
+      setCurrentIndex(0);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      if (input === wordsOfCode[currentIndex]) {
+        setInput("");
+        setCurrentIndex(currentIndex + 1);
+      }
+      if (currentIndex >= wordsOfCode.length) {
+        setGame(false);
+      }
+    }
+  };
   return (
-    <div className="container mx-auto p-2 items-center">
-      <div className="bg-grey-500 box-border h-60 w-100 p-4 border-2 rounded-3xl shadow-md mx-auto flex justify-center items-center">
-        <p>Tekst który użytkownik widzi i ma za zadanie go wpisać</p>
+    <div className="container mx-auto p-2 flex  items-center flex-col border-4 h-screen w-screen">
+      <div className="border-4 h-28 w-1/2 flex justify-center items-center rounded-md">
+        {game && currentIndex < wordsOfCode.length
+          ? wordsOfCode[currentIndex]
+          : ""}
       </div>
-      <div className="bg-grey-500 box-border h-60 w-100 p-4 border-4 shadow-md mx-auto flex justify-center items-center">
-        <p>Tekst który użytkownik widzi i ma za zadanie go wpisać</p>
-      </div>
+      <input
+        onChange={handleOnChange}
+        onKeyDown={handleKeyDown}
+        value={input}
+        type="text"
+        placeholder="Write here!"
+        className="border-4"
+      />
+      <button onClick={handleGameStart} className="border-2 w-16">
+        Start
+      </button>
     </div>
   );
 }
